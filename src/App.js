f8fe589ae25bbbd422f34ff6e91b58b6664f9466
Rajvira10 <72084907+Rajvira10@users.noreply.github.com>
2021-04-30 19:14:25 +0600

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Card from "./Card";
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Person from "./Person";
+import './index';
 
 function App() {
   return (
